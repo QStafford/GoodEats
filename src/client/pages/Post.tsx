2c81bc61d0e57f8e0f44e0afe112af993b13aca5
Quincy S <xXQS316Xx@gmail.com>
2021-11-29 16:06:44 -0600

    Barebone Forms no CSS..look kinda bad ngl

--- src/client/pages/Post.tsx
+++ src/client/pages/Post.tsx
@@ -4,22 +4,22 @@ const Post: React.FC<PostProps> = (props) => {
 	return (
 		<>
 			<h1>Post</h1>
-            <div class="mb-3">
+            <div className="mb-3">
                 <label for="restaurantPost" class="form-label">Choose a Restaurant:</label>
-                <input type="text" class="form-control" id="restaurantPost" placeholder="Yum Yum"></input>
+                <input type="text" className="form-control" id="restaurantPost" placeholder="Yum Yum"></input>
             </div>
-            <div class="mb-3">
+            <div className="mb-3">
                 <label for="yourThoughts" class="form-label">Your thoughts:</label>
                 <textarea class="form-control" id="yourThoughts" rows="3"></textarea>
             </div>
-            <div class="input-group mb-3">
+            <div className="input-group mb-3">
                 <label for="restaurantPic" class="form-label">Choose a Photo:</label>
-                <input type="file" class="form-control" id="restaurantPic"></input>
-                <label class="input-group-text" for="restaurantPic">Post</label>
+                <input type="file" className="form-control" id="restaurantPic"></input>
+                <label className="input-group-text" for="restaurantPic">Post</label>
             </div>
-            <div class="mb-3">
+            <div className="mb-3">
                 <label for="postTags" class="form-label">Tag your post:</label>
-                <input type="text" class="form-control" id="postTags" placeholder="Tags"></input>
+                <input type="text" className="form-control" id="postTags" placeholder="Tags"></input>
             </div>
 		</>
 	
