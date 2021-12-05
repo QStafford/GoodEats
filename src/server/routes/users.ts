fca7dea730f997af7b88e556478478e506d26f2d
Rachel <reparker99@gmail.com>
2021-12-05 16:53:31 -0600

    git commit -m "Routing updates" git push

--- src/server/routes/users.ts
+++ src/server/routes/users.ts
@@ -5,18 +5,17 @@ import db from '../db';
 const router = Router();
 
 //gET API/users
-router.get('/', (req, res) => {
-    router.get('/', async (req, res) => {
+router.get('/:id?', async (req, res) => {
+    const userid = Number(req.params.id);
         try {
-            const users = await db.users.all();
-            res.json(users);
+            const [user] = await db.users.one(userid);
+            res.json(user);
         } catch (error) {
             console.log(error);
             res.status(500).json({msg: 'Ohhh you in trouble girl', error });
             
         }
      });
-});
 
 
 export default router;
